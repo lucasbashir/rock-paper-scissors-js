946712e300ae36113d6a848975df66b360014ff0
lucasbashir <lucassamson24@gmail.com>
2023-03-30 21:04:21 +0100

    Game working properly

--- script.js
+++ script.js
@@ -2,48 +2,81 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("rock").addEventListener('click', () => playGame(rockChoice(), computerChoice()));
   document.getElementById("paper").addEventListener('click', () => playGame(paperChoice(), computerChoice()));
   document.getElementById("scissors").addEventListener('click', () => playGame(scissorsChoice(), computerChoice()));
+  document.getElementById("reset").addEventListener('click', reset);
 });
+
+
+
+
+function reset() {
+  // Reset the scores to 0
+  playerScore = 0;
+  computerScore = 0;
+  tie = 0;
+  count = 0;
+
+  document.getElementById("playerscore").innerHTML = `Player Score: ${playerScore}`;
+  document.getElementById("computerscore").innerHTML = `Computer Score: ${computerScore}`;
+  document.getElementById("win").textContent = "Winner: ";
+  document.getElementById("player").textContent = "You Chose: ";
+  document.getElementById("computer").textContent = "Computer Chose: ";
+  document.getElementById("tie").innerHTML = `Tie: ${tie}`;
+
+  //show game buttons
+  document.getElementById("rock").disabled = false;
+  document.getElementById("paper").disabled = false;
+  document.getElementById("scissors").disabled = false;
+
+  // Hide the "Reset" button
+  document.getElementById("reset").style.display = "none";
+}
 
 
 let computerScore = 0;
 let playerScore = 0;
+let tie = 0;
+let count = 0;
 
 function playGame(player, computer) {
   limit = 5;
 
+
   for(let i=0; i < limit; i++) {
+    count++;
+    if(count < limit) {
+      document.getElementById("reset").style.display = 'none';
+    } else if(count === limit) {
+      document.getElementById("rock").disabled = true;
+      document.getElementById("paper").disabled = true;
+      document.getElementById("scissors").disabled = true;
+      document.getElementById("reset").style.display = 'block';
+    }
+
+    if(count === limit && playerScore > computerScore) {
+      document.getElementById("win").innerHTML = "Winner: Player";
+    }
+    if(count === limit && playerScore < computerScore) {
+      document.getElementById("win").innerHTML = "Winner: Computer";
+    }
+    if((count === limit && playerScore === computerScore) || (playerScore === tie && playerScore != computerScore)){
+      document.getElementById.innerHTML = "It is a tie!"
+    }
+
+
     if(player === "rock" && computer === "rock"){
-      let s = document.getElementById("win")
-      s.textContent = "It is a tie";
+      tie++;
+      document.getElementById("tie").innerHTML = `Tie Count: ${tie}`;
       break;
-      
-    } else if(player === "rock" && computer === "scissors") {
-      let s = document.getElementById("win")
+
+    } else if(player === "rock" && computer === "scissors" || player === "scissors" && computer === "paper"
+    || player === "paper" && computer === "rock") {
       playerScore++;
-      document.getElementById("playerscore").innerHTML = playerScore;
-      s.textContent = "You won";
-      console.log(playerScore);
+      document.getElementById("playerscore").innerHTML = `Player Score: ${playerScore}`;
       break;
-      
-    } else if(player === "scissors" && computer === "paper") {
-      playerScore++;
-      document.getElementById("playerscore").innerHTML = playerScore;
-      let s = document.getElementById("win")
-      s.textContent = "You won";
-      console.log(playerScore);
-      break;
-    } else if(player === "paper" && computer === "rock") {
-      playerScore++;
-      document.getElementById("playerscore").innerHTML = playerScore;
-      let s = document.getElementById("win")
-      s.textContent = "You won";
-      console.log(playerScore);
-      break;
+
     } else {
-      let s = document.getElementById("win")
-      s.textContent = "You Lost";
       computerScore++;
-      document.getElementById("computerscore").innerHTML = computerScore;
+      document.getElementById("computerscore").innerHTML = `Computer Score: ${computerScore}`;
       console.log(computerScore)
       break;
     }
@@ -58,7 +91,7 @@ function computerChoice() {
   let rand = Math.floor(Math.random() * options.length);
   document.getElementById("computer").textContent = `Computer Chose: ${options[rand]}`
   console.log(options[rand])
-  
+
   return options[rand];
 };
 
@@ -83,6 +116,3 @@ function scissorsChoice() {
   return scissors;
 }
 
-function res() {
-  return "LLL"
-};
